3a2cb9323d19112a6ce4d4c5ea0528f8be673ad8
Lotus <cmadisonne@gmail.com>
2018-07-30 14:28:49 -0500

    Java Script If

---
+++ Ex8.js
@@ -0,0 +1,14 @@
+var userInput = prompt("Enter a number.");
+
+if(userInput == 1){
+    alert("CORRECT")
+}
+else if(userInput == 2){
+    alert("CORRECT")
+}
+else if(userInput == 3){
+    alert("CORRECT")
+}
+else{
+    alert("ERROR")
+}
