545095744b6fe8135cc25a969e04a02a437e3fc8
Lotus <cmadisonne@gmail.com>
2018-07-30 14:25:03 -0500

    JavaScript If

---
+++ Ex6.js
@@ -0,0 +1,9 @@
+var anyWord = "Ashera";
+var theSameWord = "Ashera";
+
+if (anyWord == theSameWord){
+    alert("THAT IS CORRECT!!!")
+}
+else{
+    alert("THAT IS NOT CORRECT!!!")
+}
---
+++ Ex7.js
@@ -0,0 +1,11 @@
+var userInput = prompt("Enter a number.");
+
+if(userInput > 0){
+    alert("POSITIVE")
+}
+else if(userInput < 0){
+    alert("NEGATIVE")
+}
+else if(userInput == 0){
+    alert("ZERO")
+}
